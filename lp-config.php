a2982110efbacaec76a55ec881765737c8a83166
adnan <32415271+lutfiadnan92@users.noreply.github.com>
2017-10-05 11:48:36 +0800

    Update lp-config.php

--- lp-config.php
+++ lp-config.php
@@ -1,10 +1,11 @@
 <?php
 class Config{
+	//buat variabel terlebih dahulu
 	protected static $_config = array();
-
+	//kelas yang kita gunakan adalah umum
 	public static function getConfig($key){
 		if(!self::$_config){
-			$filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'config.ini';
+			$filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'config.ini';//panggil file config yang telah dikonfigurasi
 			$config = parse_ini_file($filename);
 			if(false === $config){
 				throw new Exception('File Konfigurasi Tidak Dapat dibaca');
